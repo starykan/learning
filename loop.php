37339fa03ae03b33ed433f08ba8ec36acad948ac
staryk <staryk180289@gmail.com>
2021-11-19 16:20:39 +0300

    loop

--- loop.php
+++ loop.php
@@ -21,6 +21,7 @@ for($i=1; $i<=10; $i++) {
     echo ' <tr style="background-color: rgb('.$w.','.$w.','.$w.')">';
     echo '<td>'.$i.'</td>';
     echo '<td>'.mt_rand(0,100).'</td>';
+    echo '</tr>';
 }
 echo '</table>';
 ?>
