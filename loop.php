49dbb485cf47f1d1ff56eaec58c0e84481117416
staryk <staryk180289@gmail.com>
2021-11-19 16:10:50 +0300

    loop

---
+++ loop.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+    <style>
+        table {
+            border-collapse: collapse;
+            text-align: center;
+        }
+        td{
+            border: 1px solid black;
+            width: 40px;
+        }
+    </style>
+        </head>
+<body>
+<?php
+$w=0;
+$q=255/9;
+echo '<table>';
+for($i=1; $i<=10; $i++) {
+    $w = ($i-1) * $q;
+    echo ' <tr style="background-color: rgb('.$w.','.$w.','.$w.')">';
+    echo '<td>'.$i.'</td>';
+    echo '<td>'.mt_rand(0,100).'</td>';
+    echo '</tr>';
+}
+echo '</table>';
+?>
+</body>
+</html>
