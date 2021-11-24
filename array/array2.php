d28bf199b30d424c2d2c41bb2d84a4de11216a0f
staryk <staryk180289@gmail.com>
2021-11-24 18:54:02 +0300

    array 3

--- array/array2.php
+++ array/array2.php
@@ -13,3 +13,10 @@ foreach ($a as $k => $v){
     }
 }
 $res = array_merge($even, $odd);
+?>
+
+<?php
+
+$a = [1,2,3,4,5,6,7,8,9];
+
+
---
+++ array/array3.php
@@ -0,0 +1,16 @@
+<?php
+error_reporting($error_level = 1);
+$a = [[1,2,4],
+      [5,4,9],
+      [2,0,2]];
+
+$rowCount = count($a);
+$colCount = count($a[0]);
+for($j=0; $j < $colCount; $j++) {
+    $s = 1;
+    for ($i=0; $i < $rowCount; $i++){
+        $s *= $a[$i][$j];
+    }
+    if ($s == 0){ unset($s);}
+    echo $s, '<br>';
+}
