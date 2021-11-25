fc62b7e8e035290c28fc62ebe77106469bf20a6a
staryk <staryk180289@gmail.com>
2021-11-25 21:34:33 +0300

    string 4

--- string/string4.php
+++ string/string4.php
@@ -1,6 +1,6 @@
 <?php
 $text =
-    '<p>What is Symfony. Symfony is a set of PHP Components, a Web Application framework, a Philosophy, and a Community - all working together in harmony. </p>
+'<p>What is Symfony. Symfony is a set of PHP Components, a Web Application framework, a Philosophy, and a Community - all working together in harmony. </p>
 <p>Symfony Framework. The leading PHP framework to create websites and web applications. Built on top of the Symfony Components. </p>
 <p>Symfony Components. A set of decoupled and reusable components on which the best PHP applications are built, such as Drupal, phpBB, and eZ Publish. </p>
 <p>Symfony Community. A passionate group of over 600,000 developers from more than 120 countries, all committed to helping PHP surpass the impossible. </p>
