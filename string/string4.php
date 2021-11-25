6797e6fd044da68f2356fa6f2898c30e978018af
staryk <staryk180289@gmail.com>
2021-11-25 21:05:49 +0300

    string 4 in progress

--- string/string4.php
+++ string/string4.php
@@ -1,19 +1,13 @@
 <?php
-
 $text =
-'<p>What is Symfony. Symfony is a set of PHP Components, a Web Application framework, a Philosophy, and a Community - all working together in harmony. </p>
+    '<p>What is Symfony. Symfony is a set of PHP Components, a Web Application framework, a Philosophy, and a Community - all working together in harmony. </p>
 <p>Symfony Framework. The leading PHP framework to create websites and web applications. Built on top of the Symfony Components. </p>
 <p>Symfony Components. A set of decoupled and reusable components on which the best PHP applications are built, such as Drupal, phpBB, and eZ Publish. </p>
 <p>Symfony Community. A passionate group of over 600,000 developers from more than 120 countries, all committed to helping PHP surpass the impossible. </p>
 <p>Symfony Philosophy. Embracing and promoting professionalism, best practices, standardization and interoperability of applications. </p>';
 
 $text = strip_tags($text);
-$text1 = str_replace(array(',','.'),' ', $text);
-$arr = explode(" ", $text1);
-$max = $arr[0];
-for ($i=0; $i<count($arr); $i++) {
-    if(strlen($arr[$i]) > strlen($max)){
-        $max = $arr[$i];
-    }
+
+foreach (count_chars($text, 1) as $i => $val) {
+    echo "\"" , chr($i) , "\" встречается в строке $val раз(а).\n";
 }
-echo $max;
