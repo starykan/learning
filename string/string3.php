09d3574c2fa6b3227cadf75c93861ecf2fce1b29
staryk <staryk180289@gmail.com>
2021-11-27 17:29:14 +0300

    string 3 fixed

--- string/string3.php
+++ string/string3.php
@@ -5,15 +5,18 @@ $text =
 <p>Symfony Framework. The leading PHP framework to create websites and web applications. Built on top of the Symfony Components. </p>
 <p>Symfony Components. A set of decoupled and reusable components on which the best PHP applications are built, such as Drupal, phpBB, and eZ Publish. </p>
 <p>Symfony Community. A passionate group of over 600,000 developers from more than 120 countries, all committed to helping PHP surpass the impossible. </p>
-<p>Symfony Philosophy. Embracing and promoting professionalism, best practices, standardization and interoperability of applications. </p>';
+<p>Symfony Philosophy. Embracing and promoting professionalism, best practices, standardization and interoperabilit of applications. </p>';
 
 $text = strip_tags($text);
 $text1 = str_replace(array(',','.'),' ', $text);
 $arr = explode(" ", $text1);
-$max = $arr[0];
+$max = [$arr[0]];
 for ($i=0; $i<count($arr); $i++) {
-    if(strlen($arr[$i]) > strlen($max)){
-        $max = $arr[$i];
+    if(strlen($arr[$i]) > strlen($max[0])){
+        $max = [$arr[$i]];
+    }
+    elseif (strlen($arr[$i]) == strlen($max[0])){
+        $max []= $arr[$i];
     }
 }
-echo $max;
+var_dump($max);
